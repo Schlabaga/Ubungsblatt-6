dc3a9a9f4a0c9861274eca16e3ab271b06742492
Schlabaga <78259357+Schlabaga@users.noreply.github.com>
2024-12-09 15:02:49 +0100

    Subclass and everything

---
+++ src/Muskikinteressierte.java
@@ -0,0 +1,24 @@
+public class Muskikinteressierte {
+
+
+};
+
+public class Musiker extends Muskikinteressierte {
+
+};
+
+public class Fan extends Muskikinteressierte {
+
+};
+
+public class Rockstar extends Musiker {
+
+};
+
+public class Konzertpianist extends Musiker {
+
+};
+
+public class Bassist extends Rockstar {
+
+};
